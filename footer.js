2c2413716418a80775a503d5dc44211a877ba0c6
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2021-12-14 16:46:45 -0800

    initializes the repo

---
+++ footer.js
@@ -0,0 +1,20 @@
+const pageFooter = document.querySelector(".page-bottom");
+pageFooter.innerHTML = `<section class="socials-links-section">
+    <div class="socials-links">
+        <img src="/personal-site-rough-assets/iconmonstr-github-3-240 copy.png" alt="GitHub logo of a cat-octopus in a square" class="socials-icon">
+        <a href="https://github.com/cstowitts" target="_blank" class="socials-links-hyperlink">GitHub</a>
+    </div>
+    <div class="socials-links">
+        <img src="/personal-site-rough-assets/iconmonstr-linkedin-3-240 copy.png" alt="LinkedIn logo of the letters i and n in a square" class="socials-icon">
+        <a href="https://www.linkedin.com/in/cella-stowitts/ target="_blank" class="socials-links-hyperlink">LinkedIn</a>
+    </div>
+    <div class="socials-links">
+        <img src="/personal-site-rough-assets/iconmonstr-twitter-3-240 copy.png" alt="twitter logo of a bird in a square" class="socials-icon">
+        <a href="https://twitter.com/time2Cellabrate" target="_blank" class="socials-links-hyperlink">Twitter</a>
+    </div>
+</section>
+<div class="email-link bottom">
+    <a href="mailto:cellachungstowitts@gmail.com&subject=Connecting via your site" class="email-me-link">CellaChungStowitts@gmail.com</a>
+</div>`;
+        
+           
---
+++ header.js
@@ -0,0 +1,46 @@
+const pageHeader = document.querySelector(".page-top");
+pageHeader.innerHTML = `<div class="logo-div">
+<a href="index.html" class="logo">
+    CS
+</a>
+</div>
+<!-- page top right: nav bar (home, about, projects, contact, crafts for laughs) -->
+<div class="nav-div">
+<nav class="header-nav-bar">
+    <div class="nav home">
+        <a href="index.html" class="home button-link">
+            <button class="nav-button home">
+                Home
+            </button>
+        </a>
+    </div>
+    <div class="nav about">
+        <a href="About.html" class="about button-link">
+            <button class="nav-button about">
+                About
+            </button>
+        </a>
+    </div>
+    <div class="nav projects">
+        <a href="Projects.html" class="projects button-link">
+            <button class="nav-button projects">
+                Projects
+            </button>
+        </a>
+    </div>
+    <div class="nav contact">
+        <a href="Contact.html" class="contact button-link">
+            <button class="nav-button contact">
+                Contact
+            </button>
+        </a>
+    </div>
+    <div class="nav rec">
+        <a href="reCREATION.html" class="rec button-link">
+            <button class="nav-button rec">
+                reCREATIONal
+            </button>
+        </a>
+    </div>
+</nav>
+</div>`;
